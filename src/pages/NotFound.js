2cb1be21d0e462e2e1025f5f3a5a2875c8baf50a
Cerylion <fullthrottle.moran@gmail.com>
2024-07-30 19:34:53 -0600

    Login, NotFound, Post and Register page blocks created, NotFound works and links to Login

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/NotFound.css"
+import {Link} from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <div className='notfound'>
+      <h2 className='notfound-text'>Not Found</h2>
+      <p className='notfound-found'>Go to HomePage <Link to="/login">Here</Link> </p>
+    </div>
+  )
+}
+
+export default NotFound
